fbb82f47b5f85700740ce093a01689389b22af6e
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2023-09-16 16:11:25 +0300

    2.3

---
+++ dataType/strings/strings1.py
@@ -0,0 +1,9 @@
+"""
+Strings
+
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function:
+"""
