708fb86195d4603bafc816c1fe032ae6600ec1bc
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2023-09-19 22:27:48 +0300

    2.1

---
+++ basics/operators/operators1.py
@@ -0,0 +1,39 @@
+# Operators are used to perform operations on variables and values. 
+
+"""
+-Python Arithmetic Operators-
+Arithmetic operators are used with numeric values to perform common mathematical operations: 
+
++   Addition        x + y
+-   Substraction    x - y
+*   Multiplication  x * y
+/   Division        x / y
+%   Modulus         x % y
+**  Exponentiation  x ** y
+
+"""
+
+"""
+-Logical Operators-
+Logical operators are used to combine conditional statements: 
+and     Returns true if both statements are true
+or      Returns true if one of the statements is true
+not     Reverse the result, returns false if the result is true
+"""
+
+"""
+Identity Operators 
+Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+is      'Returns true if both variables are the same object'
+is not  'Returns true if both variables are not the same object'
+"""
+
+
+"""
+Membership Operators 
+Membership operators are used to test if a sequence is presented in an object:
+in       'Returns true if a sequence with the specified value is present in the object'
+not in   'Returns true if a sequence with the specified value is not present in the object'
+"""
+
+
