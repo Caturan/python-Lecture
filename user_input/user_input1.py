279d7691f28de579a4aaf8044cec59e4b8f896e6
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2023-11-18 17:57:23 +0300

    29

---
+++ user_input/user_input1.py
@@ -0,0 +1,10 @@
+"""
+    UserInput
+Python allows for user input. 
+That means we are able to ask the user for input. 
+"""
+username = input("Enter username:")
+print("Username is: " + username)
+
+# Python stops executing when it comes to the input() function, and continous when user has given some input. 
+
