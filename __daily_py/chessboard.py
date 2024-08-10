6e8049263c45749d47350cddf9d946a7a617ed8d
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2024-08-10 20:45:59 +0300

    43.13

---
+++ __daily_py/chessboard.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+def draw_chessboard():
+    chessboard = np.zeros((8, 8))
+    chessboard[1::2, ::2] = 1
+    chessboard[::2, 1::2] = 1
+
+    plt.figure(figsize=(4, 4))
+    plt.imshow(chessboard, cmap='gray', interpolation='nearest')
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.title('Chessboard', fontsize=20)
+
+    plt.show()
+draw_chessboard()
